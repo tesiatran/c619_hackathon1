874d9378b0b8da32e1aa782f3e915551cbeceded
Vannarith Sar <vannsar@gmail.com>
2019-07-17 16:39:11 -0700

    Fixed rendering of city objects

--- g2_pandemic/resources/js/gamestate.js
+++ g2_pandemic/resources/js/gamestate.js
@@ -2,9 +2,9 @@ class GameState{
   constructor(event){
     this.currentEvent = event;
     this.citiesList = {
-      blue: ["San Francisco", "Atlanta", "New York", "Madrid", "London"],
-      yellow: ["Bogota", "Santiago", "Lagos","Sao Paulo", "Johannesburg"],
-      red: ["Hong Kong", "Osaka", "Manila", "Ho Chi Minh", "Sydney"]};
+      blue: ["SanFrancisco", "Atlanta", "NewYork", "Madrid", "London"],
+      yellow: ["Bogota", "Santiago", "Lagos","SaoPaulo", "Johannesburg"],
+      red: ["HongKong", "Osaka", "Manila", "HoChiMinhCity", "Sydney"]};
     this.cityObjects = [];
     this.players = 2;
   }
@@ -14,9 +14,15 @@ class GameState{
     this.playerTwo = new Player("purple");
   }
 
-  createCities(color, containerId){
+  createCities(color){
     for ( var count = 0; count < this.citiesList[color].length; count++){
-      this.cityObjects.push(new BuildCity(this.citiesList[color][count], color, containerId));
+      this.cityObjects.push(new BuildCity(this.citiesList[color][count], color, this.citiesList[color][count]));
+    }
+  }
+
+  renderAllCities(){
+    for (var count = 0; count < this.cityObjects.length; count++){
+      this.cityObjects[count].render("100px", "100px");
     }
   }
 }
