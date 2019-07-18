9bb1b0cac84c08e229ab05982b92a33c047449bf
Tesia Tran <tesiatran88@gmail.com>
2019-07-17 17:23:30 -0700

    working on deck controls and added intitialize app file

---
+++ g2_pandemic/resources/js/deck-control.js
@@ -0,0 +1,15 @@
+class PlayerDeck{
+   constructor(){
+      this.cards = [];
+   }
+
+   //will interact with startGame() in GameState
+}
+
+class InfectionDeck{
+   constructor(){
+      this.cards = [];
+   }
+
+
+}
