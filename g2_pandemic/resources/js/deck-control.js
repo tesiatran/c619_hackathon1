d3f5599a685ea1df5ce4d6433e88c22ede14d907
Tesia Tran <tesiatran88@gmail.com>
2019-07-18 09:14:08 -0700

    completed creating a card and adding it to deck

--- g2_pandemic/resources/js/deck-control.js
+++ g2_pandemic/resources/js/deck-control.js
@@ -1,15 +1,41 @@
-class PlayerDeck{
-   constructor(){
-      this.cards = [];
-   }
+// class PlayerDeck{
+//    constructor(){
+//       this.playerCardDeck = [];
+//    }
 
-   //will interact with startGame() in GameState
-}
+//    createDeck(){
+//       for(var index = 0; index < 10; index++){
+//          var card = new Card();
+//          card.getDisease();
 
-class InfectionDeck{
-   constructor(){
-      this.cards = [];
-   }
+//          var deck = new Deck();
+//          var newCard = deck.addCard(card);
 
 
-}
+
+//       }
+
+//       createDeck(){
+//    var newDeck = [];
+
+//    var card = new Card();
+//    card.getDisease();
+
+//    var deck = new Deck();
+//    deck.addCard(card);
+
+//    for(var index = 0; index < 10; index++){
+//       this.cardDeck.push(cardToAdd);
+//    }
+// }
+//    }
+// }
+
+// class DiseaseDeck{
+//    constructor(){
+//       this.diseaseCardDeck = [];
+//    }
+
+
+
+// }
--- g2_pandemic/resources/js/main.js
+++ g2_pandemic/resources/js/main.js
@@ -3,9 +3,5 @@ $(document).ready(initializeApp)
 
 
 function initializeApp(){
-   var diseaseColorArray = ["red", "blue", "yellow"];
-   var redCitiesArray = ["HongKong", "Osaka", "Manila", "HoChiMinhCity", "Sydney"];
-   var blueCitiesArray = ["SanFrancisco", "Atlanta", "NewYork", "Madrid", "London"];
-   var yellowCitiesArray = ["Bogota", "Santiago", "SaoPaulo", "Lagos", "Johannesburg"];
 
 }
