39d2f840bbe6c7794624e6757b7b50ae7a41ee84
Vannarith Sar <vannsar@gmail.com>
2019-07-18 11:10:01 -0700

    Progress on player and gamestate objects

--- g2_pandemic/resources/js/gamestate.js
+++ g2_pandemic/resources/js/gamestate.js
@@ -1,12 +1,16 @@
 class GameState{
   constructor(event){
+    this.handleClick = this.handleClick.bind(this);
     this.currentEvent = event;
     this.citiesList = {
-      blue: ["SanFrancisco", "Atlanta", "NewYork", "Madrid", "London"],
+      blue: ["Seattle", "Atlanta","London"],
       yellow: ["Bogota", "Santiago", "Lagos","SaoPaulo", "Johannesburg"],
       red: ["HongKong", "Osaka", "Manila", "HoChiMinhCity", "Sydney"]};
     this.cityObjects = [];
     this.players = 2;
+  }
+  addClickHandler() {
+    $('#' + this.cityName).on('click', this.handleClick);
   }
 
   createPlayers(){
@@ -14,17 +18,17 @@ class GameState{
     this.playerTwo = new Player("purple");
   }
 
-  createCities(color){
-    for ( var count = 0; count < this.citiesList[color].length; count++){
-      this.cityObjects.push(new BuildCity(this.citiesList[color][count], color, this.citiesList[color][count]));
-    }
-  }
+  // createCities(color){
+  //   for ( var count = 0; count < this.citiesList[color].length; count++){
+  //     this.cityObjects.push(new BuildCity(this.citiesList[color][count], color, this.citiesList[color][count]));
+  //   }
+  // }
 
-  renderAllCities(){
-    for (var count = 0; count < this.cityObjects.length; count++){
-      this.cityObjects[count].render("100px", "100px");
-    }
-  }
+  // renderAllCities(){
+  //   for (var count = 0; count < this.cityObjects.length; count++){
+  //     this.cityObjects[count].render("100px", "100px");
+  //   }
+  // }
   //Calls functions to set up game
   startGame(){
     //Deal out cards to players
--- g2_pandemic/resources/js/player.js
+++ g2_pandemic/resources/js/player.js
@@ -7,7 +7,11 @@ class Player {
     $(playerClass).hide();
     this.renderMove();
   }
-
+  recieveCard(inputCards){
+    for (var i = 0; i < inputCards.length; i++){
+      this.cards.push(inputCards[i]);
+    }
+  }
   move(targetLocation){
     $('#' + this.location + " "+this.playerClass).hide();
     this.location = targetLocation;
@@ -16,5 +20,14 @@ class Player {
 
   renderMove(){
     $('#'+this.location+" "+this.playerClass).show();
+  }
+
+  directFlight(targetLocation){
+    if (this.cards.includes(targetLocation)){
+        this.move(targetLocation);
+        this.cards.splice(this.cards.indexOf(targetLocation), 1);
+    } else {
+      console.log("You do not have the card");
+    }
   }
 }
